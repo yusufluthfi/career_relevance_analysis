78e7d87123f47a19f72fe4332d1ee3f779ca7c91
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-07-07 16:21:55 +0700

    decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -21,10 +21,10 @@ from datetime import datetime
 
 def connect_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="data_mining_system"
+            host="sql12.freesqldatabase.com",
+            user="sql12788718", 
+            password="zh24wGnPJN", 
+            database="sql12788718" 
     )
 def play_success_sound():
     db = connect_db()
@@ -62,10 +62,10 @@ def save_model_to_db(
     jumlah_fold,
     akurasi_model,
     id_pengguna,
-    host="localhost",
-    user="root",
-    password="",
-    database="data_mining_system"
+    host="sql12.freesqldatabase.com",
+    user="sql12788718", 
+    password="zh24wGnPJN", 
+    database="sql12788718" 
 ):
     try:
         conn = mysql.connector.connect(
