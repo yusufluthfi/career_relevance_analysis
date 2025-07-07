77b10b59acb31588a544caf4b68ad4412924d3d1
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-07-07 16:28:32 +0700

    tambah_algoritma.py

--- tambah_algoritma.py
+++ tambah_algoritma.py
@@ -22,10 +22,10 @@ def main():
 
             try:
                 conn = mysql.connector.connect(
-                    host="localhost",
-                    user="root",
-                    password="",
-                    database="data_mining_system"
+                    host="sql12.freesqldatabase.com",
+                    user="sql12788718", 
+                    password="zh24wGnPJN", 
+                    database="sql12788718" 
                 )
                 cursor = conn.cursor()
 
