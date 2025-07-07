db10a22f0dc6139663e700ea6014437190c5ef79
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-07-07 16:31:36 +0700

    decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -143,7 +143,7 @@ def save_model_to_db(
         conn.close()
 
 
-def save_encoder_to_db(encoders_dict, host="localhost", user="root", password="", database="data_mining_system"):
+def save_encoder_to_db(encoders_dict,host="sql12.freesqldatabase.com",user="sql12788718",password="zh24wGnPJN",database="sql12788718"):
     """
     encoders_dict: dict dengan format {kode_encoder: (nama_encoder, encoder_object)}
     """
