a16d9af38107882cdd7f8c29b1042ebe5f12cf16
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-07-07 16:24:57 +0700

    kelola_model.py

--- kelola_model.py
+++ kelola_model.py
@@ -7,10 +7,10 @@ from datetime import datetime
 
 def connect_to_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="data_mining_system"
+            host="sql12.freesqldatabase.com",
+            user="sql12788718", 
+            password="zh24wGnPJN", 
+            database="sql12788718" 
     )
 
 def update_model(kode_model, nama_baru):
