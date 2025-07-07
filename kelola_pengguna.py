5f7ce2880ab81885e29a65336ea5d83793bdd985
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-07-07 16:25:45 +0700

    kelola_pengguna.py

--- kelola_pengguna.py
+++ kelola_pengguna.py
@@ -7,10 +7,10 @@ import CSS
 
 def connect_to_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="data_mining_system"
+            host="sql12.freesqldatabase.com",
+            user="sql12788718", 
+            password="zh24wGnPJN", 
+            database="sql12788718" 
     )
 
 def main():
